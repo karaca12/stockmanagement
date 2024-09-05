d4690733419e35b4f393bed6cabdd907c2bc4d05
Mehmet KARACA <mehmet.karaca@veripark.com>
2024-09-05 17:46:55 +0300

    lidl update

--- StockManagement.Web/Controllers/CategoriesController.cs
+++ StockManagement.Web/Controllers/CategoriesController.cs
@@ -16,21 +16,17 @@ namespace StockManagement.Web.Controllers
         }
 
 
-        // GET: Categories
         public async Task<IActionResult> Index()
         {
             return View(await _categoryService.GetAllAsync());
         }
 
-        // GET: Categories/Create
+
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Categories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Name,Id,CreatedAt,UpdatedAt,IsDeleted,DeletedAt")] CreateCategoryRequest request)
@@ -43,7 +39,6 @@ namespace StockManagement.Web.Controllers
             return View();
         }
 
-        // GET: Categories/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -64,9 +59,6 @@ namespace StockManagement.Web.Controllers
             return View(editRequest);
         }
 
-        // POST: Categories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Name,Id,CreatedAt,UpdatedAt,IsDeleted,DeletedAt")] EditCategoryRequest request)
@@ -98,7 +90,6 @@ namespace StockManagement.Web.Controllers
             return View(id);
         }
 
-        // GET: Categories/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -120,7 +111,6 @@ namespace StockManagement.Web.Controllers
             return View(deleteResponse);
         }
 
-        // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int? id)
