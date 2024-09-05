eadbcb09953553fa679055204c48d9b079167092
mehmet.karaca <mehmet.karaca@ISTTURNB661>
2024-09-05 09:24:17 +0300

    Add project files.

---
+++ StockManagement.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Pieces { get; set; }
+        public decimal Price { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
